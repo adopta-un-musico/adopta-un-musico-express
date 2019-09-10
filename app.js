8aa3a876f890b1cf2ed335b5b633150ecab20c56
Mario Espada <mario.espada77@gmail.com>
2019-09-10 18:24:24 +0200

    Solucionado error notificaciones

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const flash = require('connect-flash');
-const { notifications } = require('./middlewares');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
+const { notifications } = require("./middlewares");
 
-mongoose.connect('mongodb://localhost/proyecto', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/proyecto", { useNewUrlParser: true });
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const authRouter = require('./routes/auth');
-const homeRouter = require('./routes/home');
-const profileRouter = require('./routes/profile');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+const homeRouter = require("./routes/home");
+const profileRouter = require("./routes/profile");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'sass'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "sass"),
+    dest: path.join(__dirname, "public"),
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: true
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60, // 1 day
+      ttl: 24 * 60 * 60 // 1 day
     }),
-    secret: 'ironhack',
+    secret: "ironhack",
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  }),
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
 );
 
 app.use(flash());
@@ -56,12 +58,12 @@ app.use((req, res, next) => {
   app.locals.currentUser = req.session.currentUser;
   next();
 });
-app.use(notifications(app));
-app.use('/', indexRouter);
-app.use('/', authRouter);
-app.use('/home', homeRouter);
-app.use('/users', usersRouter);
-app.use('/profile', profileRouter);
+app.use(notifications());
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/home", homeRouter);
+app.use("/users", usersRouter);
+app.use("/profile", profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -72,11 +74,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,90 +1,84 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 const bcryptSalt = 10;
 
 const router = express.Router();
 
-router.get('/signup', (req, res, next) => {
-    res.render('signup'); 
-  });
-  
-router.post('/newUser', (req, res , next) =>{
-
-    const { email, password } = req.body;
-    if (email !== '' && password !== '') {
-        User.findOne({ email })
-          .then((user) => {
-            if (user) {
-              req.flash('error', 'El usuario que has introducido ya existe');
-              res.redirect('/signup');
-            } else {
-              const salt = bcrypt.genSaltSync(bcryptSalt);
-              const hashedPassword = bcrypt.hashSync(password, salt);
-              User.create({ email, hashedPassword })
-                .then(() => {
-                  req.flash('info', 'Usuario creado correctamente');
-                  res.redirect('/home');
-                })
-                .catch((error) => {
-                  throw error;
-                });
-            }
-          })
-          .catch((error) => {
-            req.flash('error', 'error vuelve a intentarlo');
-            res.redirect('/signup');
-          });
-      } else {
-        req.flash('error', 'Los campos no pueden estar vacios');
-        res.redirect('/signup');
-        
-      }
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
 });
 
-
-
-router.get('/login', (req, res, next) => {
-
-  res.render('login'); 
-});
-
-
-
-router.post('/login', (req, res, next) => {
-
+router.post("/newUser", (req, res, next) => {
   const { email, password } = req.body;
-  if (email !== '' && password !== '') {
+  if (email !== "" && password !== "") {
     User.findOne({ email })
-      .then((user) => {
+      .then(user => {
         if (user) {
-          if (bcrypt.compareSync(password, user.hashedPassword)) {
-            req.session.currentUser = user;
-            res.redirect('/home');
-          } else {
-            res.render('login', { error: 'usuario o contraseña incorrectos' });
-          }
+          req.flash("error", "El usuario que has introducido ya existe");
+          res.redirect("/signup");
         } else {
-          res.redirect('/signup');
+          const salt = bcrypt.genSaltSync(bcryptSalt);
+          const hashedPassword = bcrypt.hashSync(password, salt);
+          User.create({ email, hashedPassword })
+            .then(() => {
+              req.flash("info", "Usuario creado correctamente");
+              res.redirect("/home");
+            })
+            .catch(error => {
+              throw error;
+            });
         }
       })
-      .catch(() => {
-        res.render('login', { error: 'error vuelve a intentarlo' });
+      .catch(error => {
+        req.flash("error", "error vuelve a intentarlo");
+        res.redirect("/signup");
       });
   } else {
-    res.render('login', { error: 'Los campos no pueden estar vacios' });
+    req.flash("error", "Los campos no pueden estar vacios");
+    res.redirect("/signup");
   }
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
 
-router.get('/logout', (req, res, next) => {
-  req.session.destroy((err) => {
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  if (email !== "" && password !== "") {
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          if (bcrypt.compareSync(password, user.hashedPassword)) {
+            req.session.currentUser = user;
+            res.redirect("/home");
+          } else {
+            req.flash("error", "Usuario o contraseña inmcorrectos");
+            res.redirect("/login");
+          }
+        } else {
+          res.redirect("/signup");
+        }
+      })
+      .catch(() => {
+        req.flash("error", "Vuelve a intentarlo");
+        res.redirect("/login");
+      });
+  } else {
+    req.flash("error", "Los campos no pueden estar vacios");
+    res.redirect("/login");
+  }
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(err => {
     if (err) {
       next(err);
     }
-    res.redirect('/login');
+    res.redirect("/login");
   });
 });
 
