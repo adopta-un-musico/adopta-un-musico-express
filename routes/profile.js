bec9a1842bac6bbce61cfe1926de2f8da36c8292
Mario Espada <mario.espada77@gmail.com>
2019-09-12 19:02:17 +0200

    FIX list all bands, and fix check if is my profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,41 +1,43 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/:userId', function(req, res, next) {
-    const { userId } = req.params;
+router.get("/:userId", function(req, res, next) {
+  const { userId } = req.params;
 
-    User.findById(userId)
-    .then((user)=>{
-        if(user){
-            res.render('profile', {user});
-        }else{
-            console.log("Usuario no encontrado");
-        }
+  User.findById(userId)
+    .then(user => {
+      if (user) {
+        const isMe = req.session.currentUser._id === user._id.toString();
+        console.log(isMe, req.session.currentUser._id, user._id);
+        res.render("profile", { user, isMe });
+      } else {
+        console.log("Usuario no encontrado");
+      }
     })
-    .catch((next));
+    .catch(next);
 });
-router.get('/update/:userId', function(req, res, next) {
-    const { userId } = req.params;
+router.get("/update/:userId", function(req, res, next) {
+  const { userId } = req.params;
 
-    User.findById(userId)
-    .then((user)=>{
-        res.render('update', { user });
+  User.findById(userId)
+    .then(user => {
+      res.render("update", { user });
     })
     .catch(next);
 });
 
-router.post('/:userId', function(req, res, next) {
-    const { userId } = req.params;
-    const { email, nickname }  = req.body;
+router.post("/:userId", function(req, res, next) {
+  const { userId } = req.params;
+  const { email, nickname } = req.body;
 
-    User.findByIdAndUpdate(userId, { email: email, nickname: nickname })
-    .then((user) =>{
-        console.log(user);
-        req.flash('info', 'Perfil Actualizado correctamente');
-        res.redirect(`/profile/${userId}`);
+  User.findByIdAndUpdate(userId, { email: email, nickname: nickname })
+    .then(user => {
+      console.log(user);
+      req.flash("info", "Perfil Actualizado correctamente");
+      res.redirect(`/profile/${userId}`);
     })
     .catch(next);
 });
