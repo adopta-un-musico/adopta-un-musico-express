fcedc9f54fa689357c16ccd94ce8c28f992cb9d0
Mario Espada <mario.espada77@gmail.com>
2019-09-16 14:59:29 +0200

    ADD Se añade esta escribiendo en el chat de la banda

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,7 @@
 $(function () {
     const username = $('#username').text();
     const message = document.getElementById('text');
+    const userTyping = document.getElementById('typingu');
     var socket = io();
     $('form').submit(function(e){
       e.preventDefault();
@@ -14,11 +15,26 @@ $(function () {
      // }else{
        //   socket.emit('escribiendo', {username})
      // }
-
+      
 
     });
+
     socket.on('chat message', function(msg){
         $('#messages').append($('<p>').text(msg.username + ": " + msg.message));
       });
+    
+    message.addEventListener('keypress', () => {
+        socket.emit('typing', {username, message: "is typing..."})
+      });  
 
-  });
+    socket.on('isTyping', data =>{
+      userTyping.innerText = data.username + " " + data.message;
+    });
+
+    message.addEventListener('keyup', () =>{
+      socket.emit('stopTyping');
+    });
+    socket.on('userstopTyping', () =>{
+      userTyping.innerHTML = "";
+    });
+});
