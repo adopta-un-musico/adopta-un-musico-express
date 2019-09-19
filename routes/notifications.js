0111a09699b3476f3ce1f6541b45418ec9c1c6cf
Mario Espada <mario.espada77@gmail.com>
2019-09-19 17:53:32 +0200

    FIX notifications system

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const Notifications = require('../models/Notifications');
+const express = require("express");
+const Notifications = require("../models/Notifications");
 
 const router = express.Router();
 
-router.get('/:userId/all', async  (req, res, next) => {
+router.get("/:userId/all", async (req, res, next) => {
   const { userId } = req.params;
 
   try {
-    const notifications = await Notifications.find( {receiver: userId } ).populate('sender_band');
+    const notifications = await Notifications.find({
+      receiver: userId,
+      visited: 0
+    }).populate("sender_band");
+    console.log(notifications);
     if (notifications) {
-      if (notifications.visited === 0 || notifications[0].visited === 0) {
-        res.render('all_notifications', { notifications });
-      } else {
-        const noNotification = 'No hay notificaciones!';
-        res.render('all_notifications', { noNotification });
-      }
+      res.render("all_notifications", { notifications });
     } else {
-      const noNotification = 'No hay notificaciones!';
-      res.render('all_notifications', { noNotification });
+      const noNotification = "No hay notificaciones!";
+      res.render("all_notifications", { noNotification });
     }
   } catch (error) {
     next(error);
   }
 });
 
-router.get('/:notificationId/visited', async  (req, res, next) => {
+router.get("/:notificationId/visited", async (req, res, next) => {
   const { notificationId } = req.params;
 
   try {
-    const notification = await Notifications.findByIdAndUpdate(notificationId, { visited: 1 });
+    const notification = await Notifications.findByIdAndUpdate(notificationId, {
+      visited: 1
+    });
     // eslint-disable-next-line no-underscore-dangle
     res.redirect(`/notifications/${req.session.currentUser._id}/all`);
   } catch (error) {
