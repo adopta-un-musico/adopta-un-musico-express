26d1a3a8b0b848495db98f2c2d2ca0e00dfb98f9
Mario Espada <mario.espada77@gmail.com>
2019-09-22 16:56:27 +0200

    ADD query get bands by location in home

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
+const Band = require('../models/Band');
 
 const router = express.Router();
 
@@ -7,6 +8,8 @@ router.get('/', async (req, res, next) => {
   try {
     // eslint-disable-next-line no-underscore-dangle
     const user = await User.findById(req.session.currentUser._id);
+    const bands = await Band.find({location: user.location});
+    console.log(bands, "user: " + user);
     res.render('home', { user });
   } catch (error) {
     next(error);
