ea9016806ef5fa6576d0cbedab869b9ae2e555c0
Elena Piaggio <helenamercury@gmail.com>
2019-09-26 19:19:14 +0200

    ADD initial version of search of users

--- routes/search.js
+++ routes/search.js
@@ -3,18 +3,16 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-/* GET search page. */
-router.get('/', (req, res, next) => {
-  res.render('search', { title: 'Search' });
-});
-
 router.post('/', async (req, res, next) => {
-  console.log('dentro de ruta /search');
   const { search } = req.body;
   console.log(search);
-
-  res.redirect('/search');
+  try {
+    const users = await User.find({ nickname: search });
+    console.log(users);
+    res.render('search', { users });
+  } catch (error) {
+    next(error);
+  }
 });
-
 
 module.exports = router;
