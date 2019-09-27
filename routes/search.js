5008e7b78a27927c5d6a622c18ba1158d459d13d
Elena Piaggio <helenamercury@gmail.com>
2019-09-28 00:56:18 +0200

    add search style

--- routes/search.js
+++ routes/search.js
@@ -5,10 +5,8 @@ const router = express.Router();
 
 router.post('/', async (req, res, next) => {
   const { search } = req.body;
-  console.log(search);
   try {
     const users = await User.find({ nickname: search });
-    console.log(users);
     res.render('search', { users });
   } catch (error) {
     next(error);
